1e4541bc1c8f3aeda72dedcc9b635b48a3e1e1bb
NessTest-dev <141342048+NessTest-dev@users.noreply.github.com>
2023-08-07 14:28:22 +0300

    a

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
+const hostname = "127.0.0.1"; // Your server ip address
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const version = "3,000,000";
 
-app.get("/hi", (req, res) => {
-  res.send("Hi!");
+app.get("/", (req, res) => {
+  // set response content
+  res.send(`<html>
+                    <body>
+                        <h1 style="color:blue;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
+                        <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -30%)">
+                            <img src="https://picsum.photos/400/400?random=1">
+                        </div>
+                    </body>
+                   </html>`);
+
+  console.log(
+    `[Version ${version}]: New request => http://${hostname}:${port}` + req.url
+  );
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(
+    `[Version ${version}]: Server running at http://${hostname}:${port}/`
+  );
 });
